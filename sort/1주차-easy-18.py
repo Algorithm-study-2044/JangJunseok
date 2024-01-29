f678b3318dbac17eae8f7740783adef1109664f8
newtonjjang <newtonjjang@naver.com>
2024-01-29 17:03:16 +0900

    1주차 - 18번 풀이 다시

--- sort/1주차-easy-18.py
+++ sort/1주차-easy-18.py
@@ -11,30 +11,35 @@ class Solution(object):
         
         return result
     
-#정답코드.
-def fourSum(self, nums, target):
-    def findNsum(nums, target, N, result, results):
-        if len(nums) < N or N < 2 or target < nums[0]*N or target > nums[-1]*N:  # early termination
+#정답코드. pass. two pointer, recursion, backtracking 을 활용한 문제.
+
+class Solution(object):
+    def fourSum(self, nums, target):
+        result = []
+        self.findSums(sorted(nums),target,4,[],result)
+        return result
+
+    def findSums(self,arr,target,N,current,result):
+        # two pointers
+        if N<2 or len(arr) < 2 or target < arr[0] * N or target > arr[-1] * N:
             return
-        if N == 2: # two pointers solve sorted 2-sum problem
-            l,r = 0,len(nums)-1
+        if N == 2:
+            l,r = 0,len(arr)-1
             while l < r:
-                s = nums[l] + nums[r]
-                if s == target:
-                    results.append(result + [nums[l], nums[r]])
+                val = arr[l] + arr[r]
+                if val == target:
+                    result.append(current + [arr[l], arr[r]])
                     l += 1
-                    while l < r and nums[l] == nums[l-1]:
-                        l += 1
-                elif s < target:
+                    while l < r and arr[l] == arr[l-1]:
+                        l += 1 
+                elif val < target:
                     l += 1
+                    # while l > 0 and arr[l] == arr[l-1]:
+                    #     l += 1
                 else:
                     r -= 1
-        else: # recursively reduce N
-            for i in range(len(nums)-N+1):
-                if i == 0 or (i > 0 and nums[i-1] != nums[i]):
-                    findNsum(nums[i+1:], target-nums[i], N-1, result+[nums[i]], results)
-
-    results = []
-    findNsum(sorted(nums), target, 4, [], results)
-    return results
+        else:
+            for i in range(0,len(arr)-N+1):
+                if i == 0 or (i>0 and arr[i] != arr[i-1]):
+                    self.findSums(arr[i+1:],target-arr[i],N-1,current+[arr[i]],result)
 			
